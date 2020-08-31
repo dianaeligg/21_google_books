ad2fa5d73f79f0cb47c9d3a600e75bc9f58c9bed
Jorge Guzman <jorguzman100@gmail.com>
2020-08-31 18:34:45 +0300

    handleInputChange working

--- client/src/components/BookSearch/index.js
+++ client/src/components/BookSearch/index.js
@@ -1,19 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Card, Form, FormControl, Button } from 'react-bootstrap';
-import './BookSearch.css'
+import './BookSearch.css';
+import GoogleAPI from "../../utils/GoogleAPI";
 
 const BookSearch = () => {
 
+    const [searchState, setSearchState] = useState({
+        search: ''
+    });
+    const [resultsState, setResultsState] = useState();
+
+    const searchBooks = query => {
+        GoogleAPI.search(query)
+            .then(res => {
+                console.log('res: ', res);
+                console.log('res.data : ', res.data);
+                setResultsState(res.data)
+            })
+            .catch(err => console.log(err));
+    };
+
+    const handleInputChange = e => {
+        const name = e.target.name;
+        const value = e.target.value;
+        setSearchState({
+            [name]: value
+        });
+    };
+
+    const handleFormSubmit = e => {
+        e.preventDefault();
+        searchBooks(searchState.search);
+    };
+
+
     return (
-            <Card className='my-4'>
-                <Card.Header className='py-1 alert alert-primary'>Book Search</Card.Header>
-                <Card.Body>
-                    <Form inline>
-                        <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-                        <Button variant="outline-primary">Search</Button>
-                    </Form>
-                </Card.Body>
-            </Card>
+        <Card className='my-4'>
+            <Card.Header className='py-1 alert alert-primary'>Book Search</Card.Header>
+            <Card.Body>
+                <Form inline>
+                    <FormControl
+                        type="text"
+                        placeholder="Search"
+                        className="mr-sm-2"
+                        name="search"
+                        value={searchState.search}
+                        onChange={handleInputChange}
+                    />
+                    {console.log('searchState: ', searchState.search)}
+                    <Button
+                        variant="outline-primary"
+                        onClick={handleFormSubmit}
+                    >Search</Button>
+                </Form>
+            </Card.Body>
+        </Card>
     );
 }
 
