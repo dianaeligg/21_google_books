b02034d96586ae86e72477faeb1cbb21dbae4a9b
Jorge Guzman <jorguzman100@gmail.com>
2020-09-02 14:58:33 +0300

    Google Books API moved from the Front to the Back so can hide API key

--- client/src/utils/GoogleAPI.js
+++ client/src/utils/GoogleAPI.js
@@ -1,14 +1,10 @@
 import axios from "axios";
 
 
-// GOOGLE_BOOKS_API_KEY='AIzaSyBX4JrAbq2KAN_83CWsq_-59V-DGAdO8g4'
-
-const BASEURL = 'https://www.googleapis.com/books/v1/volumes?q=';
-const APIKEY = "AIzaSyBX4JrAbq2KAN_83CWsq_-59V-DGAdO8g4";
 
 // Export an object with a "search" method that searches the Giphy API for the passed query
 export default {
   search: function(query) {
-    return axios.get(BASEURL + query + '&key=' + APIKEY);
+    return axios.get('/api/Google_API/' + query);
   }
 };
